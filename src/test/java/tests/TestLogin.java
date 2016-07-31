7f08968231f97f2aee63cc5adce86cfdf33e72df
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-07-31 18:06:49 -0400

    Added initial page object and test for login

--- src/test/java/tests/TestLogin.java
+++ src/test/java/tests/TestLogin.java
@@ -3,30 +3,33 @@ package tests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pageObjects.Login;
+import static org.junit.Assert.*;
 
 public class TestLogin {
 
     private WebDriver driver;
+    private Login login;
 
     @Before
     public void setUp() {
         driver = new FirefoxDriver();
+        login = new Login(driver);
     }
 
     @Test
     public void succeeded() {
-        driver.get("http://the-internet.herokuapp.com/login");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("button")).click();
+        login.with("tomsmith", "SuperSecretPassword!");
+        assertTrue("Success message not present", login.successMessagePresent());
     }
 
+
     @After
-    public void tearDown() {
+    public void testDown() {
         driver.quit();
     }
+
 
 }
