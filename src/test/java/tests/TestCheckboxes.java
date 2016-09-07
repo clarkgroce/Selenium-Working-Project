18aae121c9947ce38897b6aa1109814b4ac3999e
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-09-07 18:10:10 -0400

    Added additional test to populate checkboxes and assert on current status

--- src/test/java/tests/TestCheckboxes.java
+++ src/test/java/tests/TestCheckboxes.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -42,6 +44,18 @@ public class TestCheckboxes extends Base {
         driver.get("http://the-internet.herokuapp.com/checkboxes");
         WebElement checkbox = driver.findElement(By.cssSelector("form input:nth-of-type(2)"));
         assertThat(checkbox.isSelected(), is(true));
+    }
+
+    //Loop through checkboxes clicking each one, then assert on .isSelected
+    @Test
+    public void populateCheckboxes() throws Exception {
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("input[type=\"checkbox\"]"));
+        for (WebElement checkbox : checkboxes ) {
+            checkbox.click();
+        }
+        WebElement checkbox2 = driver.findElement(By.cssSelector("form input:nth-of-type(1)"));
+        assertThat(checkbox2.isSelected(), is(true));
     }
 
 }
